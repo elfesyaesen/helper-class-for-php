2d56a9ed80808797fadab003c25390b0eeeb8ede
elfesyaesen <web.elfesyaesen@gmail.com>
2024-05-31 17:05:49 +0300

    add price method

--- helper.php
+++ helper.php
@@ -1,7 +1,7 @@
 <?php 
 class Helper
 {
-    static function banknoteBuying($currencyCode, $type = 'buying')
+    public static function banknoteBuying($currencyCode, $type = 'buying')
     {
         $request = simplexml_load_file('http://www.tcmb.gov.tr/kurlar/today.xml');
 
